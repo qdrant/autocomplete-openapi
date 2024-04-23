d5a3b00b02ecffd50b0abad41cde5dc36e1596f4
Kartik Gupta <84975264+kartik-gupta-ij@users.noreply.github.com>
2024-04-24 00:40:12 +0530

    Autocomplete Json body value for enum type (#5)
    * Autocomplete Json body value for enum type
    
    * one more test to be sure
    
    ---------
    
    Co-authored-by: generall <andrey@vasnetsov.com>

--- src/autocomplete.js
+++ src/autocomplete.js
@@ -5,76 +5,94 @@ import { AutocompleteTrie } from "./trie-completion.js";
 import { partialParseJson } from "./json-parser.js";
 import { tokenizeHeader } from "./parse-request-header.js";
 
-
 export class OpenapiAutocomplete {
-    constructor(openapi, collections) {
-        this.openapi = openapi;
-        this.extractor = new OpenAPIExtractor(openapi);
+  constructor(openapi, collections) {
+    this.openapi = openapi;
+    this.extractor = new OpenAPIExtractor(openapi);
 
-        this.methods = this.extractor.getAllMethods();
+    this.methods = this.extractor.getAllMethods();
 
-        this.trieCompletion = new AutocompleteTrie();
+    this.trieCompletion = new AutocompleteTrie();
 
-        let specialFoo = {
-            "{collection_name}": [
-                // match any word by regex
-                (token) => token == "{collection_name}" || token.match(/\w+/),
-                // complete with all words
-                (token) => collections.filter((collection) => collection.startsWith(token)),
-            ]
-        }
+    let specialFoo = {
+      "{collection_name}": [
+        // match any word by regex
+        (token) => token == "{collection_name}" || token.match(/\w+/),
+        // complete with all words
+        (token) =>
+          collections.filter((collection) => collection.startsWith(token)),
+      ],
+    };
 
-        for (let method of this.methods) {
-            let requestString = `${method.method.toUpperCase()} ${method.path}`;
-            let tokens = tokenizeHeader(requestString);
-            this.trieCompletion.addPath(tokens, specialFoo, method.body);
-        }
+    for (let method of this.methods) {
+      let requestString = `${method.method.toUpperCase()} ${method.path}`;
+      let tokens = tokenizeHeader(requestString);
+      this.trieCompletion.addPath(tokens, specialFoo, method.body);
+    }
+  }
+
+  completeRequestHeader(requestString) {
+    let tokens = tokenizeHeader(requestString);
+    let completions = this.trieCompletion.autocomplete(tokens);
+
+    return completions;
+  }
+
+  /// Expect full request header, e.g. "POST /collections/my_collection/points/search"
+  /// And partial request body, e.g. '{"vectors": {'
+  completeRequestBody(requestHeader, requestJson) {
+    let tokens = tokenizeHeader(requestHeader);
+    let dataRef = this.trieCompletion.match(tokens);
+
+    return this.completeRequestBodyByDataRef(dataRef, requestJson);
+  }
+
+  /// Expect full dataRef, e.g. "#/components/schemas/FilterRequest"
+  /// And partial request body, e.g. '{"vectors": {'
+  completeRequestBodyByDataRef(dataRef, requestJson) {
+    if (!dataRef) {
+      return [];
     }
 
-    completeRequestHeader(requestString) {
-        let tokens = tokenizeHeader(requestString);
-        let completions = this.trieCompletion.autocomplete(tokens);
+    let jsonParsedResult = partialParseJson(requestJson);
 
-        return completions
+    let editingChunk = jsonParsedResult.getEditedChunk();
+
+    if (editingChunk.editing === "value") {
+      let result = this.extractor.allEnumValues(
+        dataRef,
+        jsonParsedResult.path,
+        editingChunk.key || "",
+        editingChunk.value || ""
+      );
+
+      if (editingChunk.value === null) {
+        // If there is no value, then we should autocomplete with open quote
+        result = result.map((s) => '"' + s + '"');
+      } else {
+        // If there is a value, then we should autocomplete with closing quote only
+
+        result = result.map((s) => s + '"');
+      }
+      return result;
     }
 
-    /// Expect full request header, e.g. "POST /collections/my_collection/points/search"
-    /// And partial request body, e.g. '{"vectors": {'
-    completeRequestBody(requestHeader, requestJson) {
-        let tokens = tokenizeHeader(requestHeader);
-        let dataRef = this.trieCompletion.match(tokens);
-        
-        return this.completeRequestBodyByDataRef(dataRef, requestJson);
+    let result = this.extractor.allProperties(
+      dataRef,
+      jsonParsedResult.path,
+      editingChunk.key || ""
+    );
+
+    if (editingChunk.key === null) {
+      // If there is no key, then we should autocomplete with open quote
+
+      result = result.map((s) => '"' + s + '": ');
+    } else {
+      // If there is a key, then we should autocomplete with closing quote only
+
+      result = result.map((s) => s + '": ');
     }
 
-    /// Expect full dataRef, e.g. "#/components/schemas/FilterRequest"
-    /// And partial request body, e.g. '{"vectors": {'
-    completeRequestBodyByDataRef(dataRef, requestJson) {
-        if (!dataRef) {
-            return [];
-        }
-
-        let jsonParsedResult = partialParseJson(requestJson);
-
-        let editingChunk = jsonParsedResult.getEditedChunk();
-
-        if (editingChunk.editing !== "key") {
-            // ToDo: also try to complete values
-            return [];
-        }
-
-        let result = this.extractor.allProperties(dataRef, jsonParsedResult.path, editingChunk.key || "");
-
-        if (editingChunk.key === null) {
-            // If there is no key, then we should autocomplete with open quote
-
-            result = result.map((s) => '"' + s + '": ');
-        } else {
-            // If there is a key, then we should autocomplete with closing quote only
-
-            result = result.map((s) => s + '": ');
-        }
-
-        return result;
-    }
+    return result;
+  }
 }
---
+++ tests/autocompleteValue.tests.js
@@ -0,0 +1,114 @@
+"use strict";
+
+import assert from "assert";
+
+import openapi from "./data/openapi.json" assert { type: "json" };
+
+import { OpenapiAutocomplete } from "../src/autocomplete.js";
+
+describe("OpenAPI autocomplete", () => {
+  let theAuto = new OpenapiAutocomplete(openapi, [
+    "my_collection",
+    "my_collection2",
+  ]);
+
+  it("autocompleteing values: create collection distance ", () => {
+    let completions = [];
+    let requestHeader = "";
+    let body = "";
+
+    requestHeader = "PUT collections/my_collection";
+    body = `
+    {
+        "vectors": {
+          "size": 4,
+          "distance": "D`;
+
+    completions = theAuto.completeRequestBody(requestHeader, body);
+    assert.ok(completions.includes('Dot"'));
+  });
+
+  it("autocompleteing values: empty autocomplete ", () => {
+    let completions = [];
+    let requestHeader = "";
+    let body = "";
+
+    requestHeader = "PUT collections/my_collection";
+    body = `
+    {
+        "vectors": {
+          "size": `
+
+    completions = theAuto.completeRequestBody(requestHeader, body);
+    assert.ok(completions.length === 0);
+  });
+
+  it("autocompleteing values: Index field schema ", () => {
+    let completions = [];
+    let requestHeader = "";
+    let body = "";
+
+    requestHeader = "PUT collections/my_collection/index";
+    body = `
+    {
+        "field_name": "name_of_the_field_to_index",
+        "field_schema": 
+        `;
+
+    completions = theAuto.completeRequestBody(requestHeader, body);
+    assert.ok(completions.includes('"keyword"'));
+  });
+  it("autocompleteing values : quantization_config scalar type ", () => {
+    let completions = [];
+
+    let requestHeader = "PUT collections/my_collection";
+    let body = `
+    {
+      "vectors": {
+          "size": 768,
+          "distance": "Cosine"
+      },
+      "optimizers_config": {
+          "memmap_threshold": 20000
+      },
+      "quantization_config": {
+          "scalar": {
+              "type": "i
+`;
+
+    completions = theAuto.completeRequestBody(requestHeader, body);
+
+    assert.ok(completions.includes('int8"'));
+  });
+
+  it("autocompleteing values : quantization_config product type ", () => {
+    let theAuto = new OpenapiAutocomplete(openapi, [
+      "my_collection",
+      "my_collection2",
+    ]);
+
+    let completions = [];
+    let requestHeader = "PUT collections/test_collection";
+    let body = `
+    {
+      "vectors": {
+          "size": 768,
+          "distance": "Cosine"
+      },
+      "optimizers_config": {
+          "memmap_threshold": 20000
+      },
+      "quantization_config": {
+          "product": {
+              "compression": "x
+`;
+
+    completions = theAuto.completeRequestBody(requestHeader, body);
+
+    assert.ok(completions.includes('x4"'));
+    assert.ok(completions.includes('x8"'));
+    assert.ok(completions.includes('x16"'));
+    assert.ok(completions.includes('x32"'));
+    assert.ok(completions.includes('x64"'));
+  });
+});
